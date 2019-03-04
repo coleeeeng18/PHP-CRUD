df195cfdb0efacce5e7b948ace5f919610652bce
Coleeen Ligasan <coleen.jade.ligasan@ph.pwc.com>
2019-03-04 17:44:28 +0800

    CRUD not complete. Edit function still in progress.

--- Source Files/Index.php
+++ Source Files/Index.php
@@ -43,8 +43,8 @@
                 <td><?php echo $row['location']; ?></td>
                 <td>
                     <a href="index.php?edit=<?php echo $row['id']; ?>"
-                        class="btn btn-success"> Edit </a>
-                    <a href="index.php?delete=<?php echo $row['id']; ?>"
+                        class="btn btn-info"> Edit </a>
+                    <a href="process.php?delete=<?php echo $row['id']; ?>"
                         class="btn btn-danger">Delete</a>
                 </td>
             </tr>
@@ -61,19 +61,28 @@
         }
 ?>
         <div class="row justify-content-center">
-            <form action="process.php" method="post">
+            <form action="process.php" method="POST">
+                <input type="hidden" name="id" value="<?php echo $id; ?>">
                 <div class="form-group">
                 <label>Name:</label>
-                <input type="text" name="name" class="form-control" value="Enter your name">
+                <input type="text" name="name" class="form-control" 
+                    value="<?php echo $name; ?>" placeholder="Enter your name">
                 </div>
 
                 <div class="form-group">
                 <label>Location: </label>
-                <input type="text" name="location" class="form-control" value="Enter your location">
+                <input type="text" name="location" class="form-control" 
+                    value="<?php echo $location; ?>" placeholder="Enter your location">
                 </div>
 
                 <div class="form-group">
-                <button type="submit" class="btn btn-primary" name="save">Save</button>
+                <?php
+                if ($update == true):
+                ?>
+                    <button type="submit" class="btn btn-info" name="update">Update</button>
+                <?php else: ?>
+                    <button type="submit" class="btn btn-primary" name="save">Save</button>
+                <?php endif; ?>
                 </div>
             </form>
         </div>
--- Source Files/process.php
+++ Source Files/process.php
@@ -4,12 +4,17 @@ session_start();
 
 $mysqli = new mysqli('localhost', 'root', '12345', 'crud') or die(mysqli_error($mysqli));
 
+$id = 0;
+$update = false;
+$name = '';
+$location = '';
+
 if (isset($_POST['save'])){
     $name = $_POST['name'];
     $location = $_POST['location'];
 
     $mysqli->query("INSERT INTO data(name, location) VALUES('$name', '$location')") or
-    die($mysqli->error);
+        die($mysqli->error);
     
     $_SESSION['message'] = "Record has been saved!";
     $_SESSION['msg_type'] = "success";
@@ -23,5 +28,31 @@ if (isset($_GET['delete'])){
 
     $_SESSION['message'] = "Record has been deleted!";
     $_SESSION['msg_type'] = "danger";
+    
     header("location: index.php");
+}
+
+if (isset($GET['edit'])){
+    $id = $_GET['edit'];
+    $update = true;
+    $result = $mysqli->query("SELECT * FROM data WHERE id=$id") or die($mysqli->error());
+
+    if (count($result)==1){
+        $row = $result->fetch_array();
+        $name = $row['name'];
+        $location = $row['location'];
+    }
+}
+
+if (isset($_POST['update'])){
+    $id = $_POST['id'];
+    $name = $_POST['name'];
+    $location = $_POST['location'];
+
+    $mysqli->query("UPDATE data SET name='$name', location='location' WHERE id=$id") or
+        die($mysqli->error);
+        $_SESSION['message'] = "Record has been updated!";
+        $_SESSION['msg_type'] = "warning";
+
+        header('location: index.php');
 }
